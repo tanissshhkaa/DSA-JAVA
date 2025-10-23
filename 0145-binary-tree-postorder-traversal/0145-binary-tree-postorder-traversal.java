d255b6b86968ab3c8494d78727345cef9738c28f
tanissshhkaa <157448845+tanissshhkaa@users.noreply.github.com>
2025-10-24 00:09:20 +0530

    Time: 0 ms (100%), Space: 41.9 MB (29.77%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,18 +13,42 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ls= new ArrayList<>();
-        postorder(root,ls);
-        return ls;  
-    }
+
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> ls= new ArrayList<>();
+//         postorder(root,ls);
+//         return ls;  
+//     }
 
 
-    public void postorder(TreeNode root, List<Integer> ls){
-        if(root==null) return;
-        postorder(root.left,ls);
-        postorder(root.right,ls);
-        ls.add(root.val);
+//     public void postorder(TreeNode root, List<Integer> ls){
+//         if(root==null) return;
+//         postorder(root.left,ls);
+//         postorder(root.right,ls);
+//         ls.add(root.val);
+//     }
+// }
+
+class Solution{
+    public List<Integer> postorderTraversal(TreeNode root){
+        List<Integer> ls=new ArrayList<>();
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode ans=root , last=null;
+        while (ans!=null || !st.isEmpty()){
+        if(ans!=null){
+            st.push(ans);
+            ans=ans.left;
+        }else{
+            TreeNode peek=st.peek();
+            if(peek.right!=null && last != peek.right){
+                ans=peek.right;
+            }else{
+                ls.add(peek.val);
+                last=st.pop();
+            }
+        }
+        }
+        return ls;
     }
 }
