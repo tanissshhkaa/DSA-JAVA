2edc891a91e2a3177c31c615258f535f3fd7df81
tanissshhkaa <157448845+tanissshhkaa@users.noreply.github.com>
2025-10-29 10:21:07 +0530

    Time: 0 ms (100%), Space: 41.8 MB (47.46%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -30,25 +30,54 @@
 //     }
 // }
 
+
+//ITERATIVE APPROACH-using 1 stack
+// class Solution{
+//     public List<Integer> postorderTraversal(TreeNode root){
+//         List<Integer> ls=new ArrayList<>();
+//         Stack<TreeNode> st=new Stack<>();
+//         TreeNode node=root , last=null;
+//         while (node!=null || !st.isEmpty()){
+//         if(node!=null){
+//             st.push(node);
+//             node=node.left;
+//         }else{
+//             TreeNode peek=st.peek();
+//             if(peek.right!=null && last != peek.right){
+//                 node=peek.right;
+//             }else{
+//                 ls.add(peek.val);
+//                 last=st.pop();
+//             }
+//         }
+//         }
+//         return ls;
+//     }
+// }
+
+//ITERATIVE APPROACH-using 2 stack
 class Solution{
     public List<Integer> postorderTraversal(TreeNode root){
+        Stack<TreeNode> st1=new Stack<>();
+        Stack<TreeNode> st2=new Stack<>();
         List<Integer> ls=new ArrayList<>();
-        Stack<TreeNode> st=new Stack<>();
-        TreeNode ans=root , last=null;
-        while (ans!=null || !st.isEmpty()){
-        if(ans!=null){
-            st.push(ans);
-            ans=ans.left;
-        }else{
-            TreeNode peek=st.peek();
-            if(peek.right!=null && last != peek.right){
-                ans=peek.right;
-            }else{
-                ls.add(peek.val);
-                last=st.pop();
+        if(root==null){
+            return ls;
+        }
+        st1.push(root);
+        while(!st1.isEmpty()){
+            root=st1.pop();
+            st2.add(root);
+            if(root.left!=null){
+                st1.push(root.left);
+            }
+            if(root.right!=null){
+                st1.push(root.right);
             }
         }
-        }
-        return ls;
+            while(!st2.isEmpty()){
+                ls.add(st2.pop().val);
+            }
+        return ls;   
     }
 }
