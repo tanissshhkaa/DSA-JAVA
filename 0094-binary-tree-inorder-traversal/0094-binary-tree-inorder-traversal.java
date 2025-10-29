bf4477f212f24c1ba17e87281b0b31c494225aaf
tanissshhkaa <157448845+tanissshhkaa@users.noreply.github.com>
2025-10-29 09:47:40 +0530

    Time: 0 ms (100%), Space: 41.7 MB (46.51%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,49 +1,21 @@
-
-import java.util.*;
-/*
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-// class Solution {
-//     public List<Integer> inorderTraversal(TreeNode root) {
-//         List<Integer> ls = new ArrayList<>();
-//         Inorder(root,ls);
-//         return ls;
-
-        
-//     }
-//     public void Inorder(TreeNode root, List<Integer> ls) {
-//         if(root==null) return;
-//         Inorder(root.left,ls);
-//         ls.add(root.val);
-//         Inorder(root.right,ls);
-//     }
-// }
-
+//Iterative Approach
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ls = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode ans = root;
-        while (ans != null|| !stack.isEmpty() ) {
-            if (ans != null) {
-                stack.push(ans);
-                ans = ans.left;
-            } else {
-                ans = stack.pop();
-                ls.add(ans.val);
-                ans = ans.right;
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode node = root;
+        while(true){
+            if(node!=null){
+                st.push(node);
+                node=node.left;
+            }
+            else{
+                if(st.isEmpty()){
+                    break;
+                }
+                node=st.pop();
+                ls.add(node.val);
+                node=node.right;
             }
         }
         return ls;
