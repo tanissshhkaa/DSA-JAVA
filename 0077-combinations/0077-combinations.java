f68453ac28f6929d754c10cd101e956993e9fa3b
tanissshhkaa <157448845+tanissshhkaa@users.noreply.github.com>
2025-10-30 17:18:30 +0530

    Time: 18 ms (77.67%), Space: 94.9 MB (28.18%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-         List<List<Integer>> ans = new ArrayList<>();
-    dfs(n, k, 1, new ArrayList<>(), ans);
-    return ans;
+         List<List<Integer>> ls = new ArrayList<>();
+    backtrack(n, k, 1, new ArrayList<>(), ls);
+    return ls;
   }
 
-  private void dfs(int n, int k, int s, List<Integer> path, List<List<Integer>> ans) {
-    if (path.size() == k) {
-      ans.add(new ArrayList<>(path));
+  private void backtrack(int n, int k, int s, List<Integer> list, List<List<Integer>> ls) {
+    if (list.size() == k) {
+      ls.add(new ArrayList<>(list));
       return;
     }
 
     for (int i = s; i <= n; ++i) {
-      path.add(i);
-      dfs(n, k, i + 1, path, ans);
-      path.remove(path.size() - 1);
+      list.add(i);
+      backtrack(n, k, i + 1, list, ls);
+      list.remove(list.size() - 1);
     }
     }
 }
