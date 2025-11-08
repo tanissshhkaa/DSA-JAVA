fcfee3241d7a2c925f42f618a5282c84a52d8c93
tanissshhkaa <157448845+tanissshhkaa@users.noreply.github.com>
2025-11-08 23:50:21 +0530

    Time: 1 ms (99.19%), Space: 45.2 MB (6.26%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -14,39 +14,46 @@
  * }
  */
 
-
 class Pair{
     TreeNode node;
     int num;
+
     Pair(TreeNode node, int num){
-    this.node=node;
-    this.num=num;
+        this.node=node;
+        this.num=num;
+    }
 }
-}
+
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
+        if(root==null) return 0;
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root, 0));
-        int width = 0;
-        while (!q.isEmpty()) {
-            int size = q.size();
-            int min = q.peek().num; // first index in this level
-            int first = 0, last = 0;
-            for (int i = 0; i < size; i++) {
-                int curr_Id = q.peek().num - min; // normalized index
-                TreeNode node = q.peek().node;
+        q.add(new Pair(root,0));
+        int width=0;
+        while(!q.isEmpty()){
+            int size=q.size();
+            int min=q.peek().num;
+            int first=0,last=0;
+            for(int i=0;i<size;i++){
+                int curr_Id=q.peek().num-min;
+                TreeNode node=q.peek().node;
                 q.poll();
-                if (i == 0) first = curr_Id;
-                if (i == size - 1) last = curr_Id;
-                if (node.left != null)
-                    q.add(new Pair(node.left, curr_Id * 2 + 1));
-                if (node.right != null)
-                    q.add(new Pair(node.right, curr_Id * 2 + 2));
+                if(i==0){
+                    first=curr_Id;
+                }
+                if(i==size-1){
+                    last=curr_Id;
+                }
+            if(node.left!=null){
+                q.add(new Pair(node.left,curr_Id*2+1));
             }
-            width = Math.max(width, last - first + 1);
+            if(node.right!=null){
+                q.add(new Pair(node.right,curr_Id*2+2));
+            }
+            }
+            width=Math.max(width,last-first+1);
         }
-        return width;
+       return width;
     }
 }
 
