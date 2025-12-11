f3d8dab4a0978e91481848b41701658f4f87efe5
tanissshhkaa <157448845+tanissshhkaa@users.noreply.github.com>
2025-12-11 11:44:45 +0530

    Time: 0 ms (100%), Space: 46.7 MB (50.25%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -5,17 +5,15 @@ class Solution {
             dfs(image,sr,sc,newColor,color);
         }
             return image;
-    }
-        
+        }
         public void dfs(int[][] image,int i,int j,int newColor,int color){
-            if(i<0 || j<0 || i>=image.length||j>=image[0].length || image[i][j]!=color){
+            if(i<0 || j<0 || i>=image.length || j>=image[0].length || image[i][j]!=color){
                 return;
             }
             image[i][j]=newColor;
             dfs(image,i+1,j,newColor,color);
-            dfs(image,i-1,j,newColor,color);
-            dfs(image,i,j+1,newColor,color);
-            dfs(image,i,j-1,newColor,color);
+             dfs(image,i-1,j,newColor,color);
+              dfs(image,i,j+1,newColor,color);
+               dfs(image,i,j-1,newColor,color);
         }
     }
-
