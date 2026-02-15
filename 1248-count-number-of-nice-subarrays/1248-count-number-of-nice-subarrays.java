ae7b77b27b49faf9b4d856eabf9349bcf84209b9
tanissshhkaa <157448845+tanissshhkaa@users.noreply.github.com>
2026-02-15 11:56:49 +0530

    Time: 14 ms (50.5%), Space: 60 MB (14.26%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-       return atMost(nums, k) - atMost(nums, k - 1);
-    }
-    private int atMost(int[] nums, int k) {
-        if (k < 0) return 0;
-        int left = 0, count = 0, oddCount = 0;
-        for (int right = 0; right < nums.length; right++) {
-            if (nums[right] % 2 == 1) {
-                oddCount++;
-            }
-            while (oddCount > k) {
-                if (nums[left] % 2 == 1) {
-                    oddCount--;
-                }
-                left++;
-            }
-            count += right - left + 1;
+public int numberOfSubarrays(int[] nums, int k) {
+return atMost(nums,k) - atMost(nums,k-1);
+}
+public int atMost(int[] nums,int k){
+    if(k<0) return 0;
+    int left=0, count=0, oddCount=0;
+    for(int right=0;right<nums.length;right++){
+        if(nums[right]%2==1){
+            oddCount++;
         }
-        return count;
+        while(oddCount>k){
+            if(nums[left]%2==1){
+                oddCount--;
+            }
+            left++;
+        }
+        count+=right-left+1;
     }
+    return count;
+}
 }
