d814ef53820895b6d69433291b752f2587ed2e64
tanissshhkaa <157448845+tanissshhkaa@users.noreply.github.com>
2026-03-23 18:44:53 +0530

    Time: 1 ms (98.52%), Space: 47.7 MB (70.59%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -6,9 +6,9 @@ class Solution {
         boolean emptyLeft=(i==0) || (flowerbed[i-1]==0);
         boolean emptyRight=(i==len-1) || (flowerbed[i+1]==0);
         if(emptyLeft && emptyRight){
-          flowerbed[i]=1;
-          n--;
-          if(n==0) return true;  
+            flowerbed[i]=1;
+            n--;
+            if(n==0) return true;
         }
     }
   }
