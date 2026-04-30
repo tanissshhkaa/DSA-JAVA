60b21ec58a5397e22d318768aa5c7a440b9fe7a7
Tanishka Singh <157448845+tanissshhkaa@users.noreply.github.com>
2026-04-30 20:12:43 +0530

    Time: 11 ms (49.52%), Space: 110.3 MB (94.05%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
@@ -2,11 +2,11 @@ class Solution {
     public int climbStairs(int n, int[] costs) {
         int[] dp = new int[3];
         for (int i = 1; i <= n; i++) {
-            int best = Integer.MAX_VALUE;
-            if (i >= 1) best = Math.min(best, dp[(i - 1) % 3] + 1);
-            if (i >= 2) best = Math.min(best, dp[(i - 2) % 3] + 4);
-            if (i >= 3) best = Math.min(best, dp[(i - 3) % 3] + 9);
-            dp[i % 3] = best + costs[i - 1];
+            int low = Integer.MAX_VALUE;
+            if (i >= 1) low = Math.min(low, dp[(i - 1) % 3] + 1);
+            if (i >= 2) low = Math.min(low, dp[(i - 2) % 3] + 4);
+            if (i >= 3) low = Math.min(low, dp[(i - 3) % 3] + 9);
+            dp[i % 3] = low + costs[i - 1];
         }
         return dp[n % 3];
     }
