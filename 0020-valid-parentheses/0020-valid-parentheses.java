1849256f6acc0d4073f24c7524778bb2ae873215
Tanishka Singh <157448845+tanissshhkaa@users.noreply.github.com>
2026-08-09 00:11:52 +0530

    Time: 3 ms (86.01%), Space: 43.1 MB (73.6%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,26 @@
-class Solution {
+class Solution {                                      
     public boolean isValid(String s) {
-      Stack<Character> st=new Stack<>();
-      for(char ch:s.toCharArray()){
-        if(ch=='(' || ch=='[' || ch=='{'){
+    Stack<Character> st=new Stack<>();
+    for(char ch:s.toCharArray()){
+        if(ch=='(' || ch=='{' || ch=='['){
             st.push(ch);
         }else{
-         if(st.isEmpty()){
-            return false;
-         }
-         char top=st.pop();
-         if(ch==')' && top!='('){
-            return false;
-         }
-         if(ch==']' && top!='['){
-            return false;
-         }
-         if(ch=='}' && top!='{'){
-            return false;
-         }
+            if(st.isEmpty()){
+                return false;
+            }
+            char top=st.pop();
+            if(ch==')' && top!='('){
+                return false;
+            }
+            if(ch=='}' && top!='{'){
+                return false;
+            }
+            if(ch==']' && top!='['){
+                return false;
+            }
         }
-      }
-      return st.isEmpty();
+    }
+    return st.isEmpty();
     }
 }
+
